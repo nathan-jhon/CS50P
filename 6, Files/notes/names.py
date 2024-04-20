75031745919fc4fc130fd031b74d0462b141a0f9
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-04-20 12:47:53 +0300

    update

--- 6, Files/notes/names.py
+++ 6, Files/notes/names.py
@@ -6,8 +6,49 @@ for i in range(3):
 	j.write(name + "\n")
 j.close()
 """
-
+# Note this code is from the cs50p class
+""" 
 name = input("name: ")
 j = open("test.txt", "a")
-j.write(name + "/n")
+j.write(name + "\n")
 j.close()
+
+"""
+
+# With
+'''
+for nati in range(3):
+	name = input("name: ")
+	with open("with.txt", "a") as i:
+		i.write(name + "\n")
+'''
+with open("with.txt", "r") as file:
+	lines = file.readlines()
+	
+for line in lines:
+	# print(f"hello, {line}") We can use 
+	print("hello", line.rstrip())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
