3dba3da3ebd98883e01e89bd91f00652acdb22b7
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-04-24 22:00:08 +0300

    W6N backup

--- 6, Files/notes/csv.py
+++ 6, Files/notes/csv.py
@@ -4,6 +4,7 @@ with open("students.csv") as file:
 	for line in sorted(file):
 		row = line.rstrip().split(",")
 		print(f"{row[0]} lives in {row[1]}") '''
+"""
 students = []
 
 with open("students.csv") as file:
@@ -16,4 +17,44 @@ def get_name(student):
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
+"""
+
+import csv
+students = []
+with open("students.csv") as file:
+	reader = csv.reader(file)
+	for row in reader:
+		students.append({"name": row[0], "home": row[1]})
+for student in sorted(students, ket=lambda student: student['name']):
+	print(f"{student['name']} is from {student['home']}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
